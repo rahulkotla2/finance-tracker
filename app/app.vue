811794ce6f5909139c76bc74678b62e9e6b500a2
Rahul Kotla <rahul.kotla@quantrium.ai>
2026-05-15 12:41:04 +0530

    Updated favicon and icon and made it PWA.

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <VitePwaManifest />
     <UApp>
     <NuxtLayout>
         <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,35 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   ssr: false,
-  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase', '@vite-pwa/nuxt'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Finance Tracker',
+      short_name: 'Finance',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: 'fn-icon.png',
+          sizes: '192x192 512x512',
+          type: 'image/svg+xml',
+          purpose: 'any maskable'
+        }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  },
   supabase: {
     redirect: false
   },
